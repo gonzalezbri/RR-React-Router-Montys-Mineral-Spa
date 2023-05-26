b7af2eea8f05bda4472be6d4facb7c091ae8d037
gonzalezbri <4717bg@gmail.com>
2023-05-25 21:32:49 -0700

    finished adding routes

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,15 @@
 
 
-export default function Packages() {
+export default function Packages(props) {
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    <ul>
+                    {props.packages.map((element,i)=><li style={{fontSize:'20px'}} key={i}>{element}</li>)}
+                    </ul>
                 </div>
                 
             </div>
